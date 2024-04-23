67fc0777af05bd48e46fe1a026faca3c2ce49fd7
CopilRacheta <m.carbunariu@yahoo.com>
2024-04-23 09:36:51 +0100

    Adding ProductsModel to the Model folder

---
+++ GreenGardens/Model/ProductsModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace GreenGardens.Model
+{
+    public class ProductsModel
+    {
+        [Key]
+        public int Guid { get; set; }
+
+        [Required,StringLength(100)]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [AllowNull]
+        public string? ImagePath { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+        
+        [Required]
+        public int ExpectedStock { get; set; }
+    }
+}
