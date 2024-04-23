5e726285b46334a6b7f785f29b0073d20f564b4c
CopilRacheta <m.carbunariu@yahoo.com>
2024-04-23 09:32:22 +0100

    Adding code to the CustomerModel

--- GreenGardens/Model/CustomersModel.cs
+++ GreenGardens/Model/CustomersModel.cs
@@ -1,6 +1,30 @@
-﻿namespace GreenGardens.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GreenGardens.Model
 {
     public class CustomersModel
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        [Display(Name ="FirstName")]
+        public string Fname { get; set; }
+
+        [Required,StringLength(100)]
+        [Display(Name ="Surname")]
+        public string Sname { get; set; }
+
+        [Required,EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required,StringLength (100)]
+        public string Password { get; set; }
+
+        [Required]
+        public string IsItAdmin { get; set; }
+
+        public int LoyalityPoints { get; set; }
+
     }
 }
